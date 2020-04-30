eba2ec8e5f9a41cc4b4efea765d16a174abf9f3a
Jonathan Hsu <jonhsu.design@gmail.com>
2020-04-29 18:50:40 -0700

    revised alerts for failed requests

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,143 +1,139 @@
-import React, { useState }  from 'react';
-import { Link } from 'react-router-dom';
-import Navbar from './navsAndMenues/Navbar';
-import { useHistory } from 'react-router-dom';
-import axios from 'axios';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Navbar from "./navsAndMenues/Navbar";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
 import * as yup from "yup";
 
 const formSchema = yup.object().shape({
-  username: yup
-    .string()
-    .required("Enter your username"),
-  password: yup
-    .string()
-    .required("Enter your password"),
+  username: yup.string().required("Enter your username"),
+  password: yup.string().required("Enter your password"),
 });
 
 const LoginForm = () => {
-
-
   const initialState = {
-    username: '',
-    password: ''
-}
+    username: "",
+    password: "",
+  };
 
   const [errors, setErrors] = useState({
     username: "",
     password: "",
   });
 
- const [credentials, setCredentials] = useState(initialState);
- const history = useHistory();
+  const [credentials, setCredentials] = useState(initialState);
+  const history = useHistory();
 
- const onChangeHandler = e => {
+  const onChangeHandler = (e) => {
     e.preventDefault();
     e.persist();
 
     yup
-    .reach(formSchema, e.target.name)
-    .validate(e.target.value)
-    .then((valid) => {
-      setErrors({
-        ...errors,
-        [e.target.name]: "",
+      .reach(formSchema, e.target.name)
+      .validate(e.target.value)
+      .then((valid) => {
+        setErrors({
+          ...errors,
+          [e.target.name]: "",
+        });
+      })
+      .catch((err) => {
+        setErrors({
+          ...errors,
+          [e.target.name]: err.errors[0],
+        });
       });
-    })
-    .catch((err) => {
-      setErrors({
-        ...errors,
-        [e.target.name]: err.errors[0],
-      });
-    });
 
     setCredentials({
-        ...credentials,
-        [e.target.name] : e.target.value
+      ...credentials,
+      [e.target.name]: e.target.value,
     });
- };
+  };
 
- const login = e => {
+  const login = (e) => {
     e.preventDefault();
     axios
-        .post('https://bw-node.herokuapp.com/auth/login', credentials)
-        .then(res => {
-            localStorage.setItem('token', JSON.stringify(res.data.token));
-            history.push('/dashboard');
-            console.log(res)
-          })
-        .catch(err => console.log(err));
-    
+      .post("https://bw-node.herokuapp.com/auth/login", credentials)
+      .then((res) => {
+        localStorage.setItem("token", JSON.stringify(res.data.token));
+        history.push("/dashboard");
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("Login failed. Please try again.");
+      });
+
     setCredentials(initialState);
-}
+  };
 
-    return(
-        <div className="blurred-bg-container">
-            <Navbar />
-  <div className="content">
-    <div className="text">
-    <div className='login-form'>
-        <h1 className='title is-2 welcome'>Login</h1>
-        
-            <form className='form' onSubmit={login}>
+  return (
+    <div className="blurred-bg-container">
+      <Navbar />
+      <div className="content">
+        <div className="text">
+          <div className="login-form">
+            <h1 className="title is-2 welcome">Login</h1>
 
-                <label htmlFor='username'>
-                        Username
-                    <div className="field">
-                        <p className="control has-icons-left">
-                        <input
-                        className='input is-large'
-                        type='text'
-                        name='username'
-                        onChange={onChangeHandler}
-                        value={credentials.username}
+            <form className="form" onSubmit={login}>
+              <label htmlFor="username">
+                Username
+                <div className="field">
+                  <p className="control has-icons-left">
+                    <input
+                      className="input is-large"
+                      type="text"
+                      name="username"
+                      onChange={onChangeHandler}
+                      value={credentials.username}
                     />
-                        <span className="icon is-small is-left">
-                            <i className="fas fa-user"/>
-                        </span>
-                        </p>
-                        {errors.username}
-                    </div>
-                </label>
+                    <span className="icon is-small is-left">
+                      <i className="fas fa-user" />
+                    </span>
+                  </p>
+                  {errors.username}
+                </div>
+              </label>
 
-                <label htmlFor='password'>
-                    Password
-                    <div className="field">
-                        <p className="control has-icons-left">
-                        <input
-                            className='input is-large'
-                            type='password'
-                            name='password'
-                            onChange={onChangeHandler}
-                            value={credentials.password}
-                        />
-                        <span className="icon is-small is-left">
-                            <i className="fas fa-lock"/>
-                        </span>
-                        </p>
-                        {errors.password}
-                    </div>
-                </label>
+              <label htmlFor="password">
+                Password
+                <div className="field">
+                  <p className="control has-icons-left">
+                    <input
+                      className="input is-large"
+                      type="password"
+                      name="password"
+                      onChange={onChangeHandler}
+                      value={credentials.password}
+                    />
+                    <span className="icon is-small is-left">
+                      <i className="fas fa-lock" />
+                    </span>
+                  </p>
+                  {errors.password}
+                </div>
+              </label>
 
-                <div className='spacer-sm'/>
-                <button 
-                    type='submit' 
-                    className='button is-danger is-large is-rounded login-btn'
-                >
-                    <i className="fas fa-lock-open"/>
-                    Login
-                </button>
-                <div className='spacer-sm'/>
-            <Link className='signup-link-2' to='/signup'>Don't have an account? Sign up here</Link>
+              <div className="spacer-sm" />
+              <button
+                type="submit"
+                className="button is-danger is-large is-rounded login-btn"
+              >
+                <i className="fas fa-lock-open" />
+                Login
+              </button>
+              <div className="spacer-sm" />
+              <Link className="signup-link-2" to="/signup">
+                Don't have an account? Sign up here
+              </Link>
             </form>
-            
+          </div>
         </div>
-       
-    </div>
 
-    <div className="blur"/>
-  </div>
-</div>
-    )
+        <div className="blur" />
+      </div>
+    </div>
+  );
 };
 
 export default LoginForm;
--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
-import Navbar from './navsAndMenues/Navbar';
-import axios from 'axios';
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import Navbar from "./navsAndMenues/Navbar";
+import axios from "axios";
 import * as yup from "yup";
 
 const formSchema = yup.object().shape({
@@ -9,186 +9,173 @@ const formSchema = yup.object().shape({
     .string()
     .min(3, "Username must be at least 3 characters")
     .required("Enter a username"),
-  email: yup
-    .string()
-    .email("Enter a valid email")
-    .required("Enter your email"),
+  email: yup.string().email("Enter a valid email").required("Enter your email"),
   password: yup
     .string()
     .matches(
       /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])/,
-      "Password must contain at least one Uppercase, one Lowercase, and a Number")
+      "Password must contain at least one Uppercase, one Lowercase, and a Number"
+    )
     .required("Enter your password"),
-  account_type: yup
-    .string(),
+  account_type: yup.string(),
 });
 
 const SignupForm = () => {
-    
-    const initialState = {
-      username: '',
-      email: '',
-      password: '',
-      account_type: ''
-    }
+  const initialState = {
+    username: "",
+    email: "",
+    password: "",
+    account_type: "",
+  };
 
-    const [errors, setErrors] = useState({
-      username: "",
-      email: "",
-      password: "",
-      account_type: "",
+  const [errors, setErrors] = useState({
+    username: "",
+    email: "",
+    password: "",
+    account_type: "",
+  });
+
+  const [formState, setFormState] = useState(initialState);
+  const history = useHistory();
+
+  const onChangeHandler = (e) => {
+    e.preventDefault();
+    e.persist();
+
+    yup
+      .reach(formSchema, e.target.name)
+      .validate(e.target.value)
+      .then((valid) => {
+        setErrors({
+          ...errors,
+          [e.target.name]: "",
+        });
+      })
+      .catch((err) => {
+        setErrors({
+          ...errors,
+          [e.target.name]: err.errors[0],
+        });
+      });
+
+    setFormState({
+      ...formState,
+      [e.target.name]: e.target.value,
     });
-    
-    const [formState, setFormState] = useState(initialState);
-    const history = useHistory();
 
-    const onChangeHandler = e => {
-        e.preventDefault();
-        e.persist();
-        
-        yup
-        .reach(formSchema, e.target.name)
-        .validate(e.target.value)
-        .then((valid) => {
-          setErrors({
-            ...errors,
-            [e.target.name]: "",
-          });
-        })
-        .catch((err) => {
-          setErrors({
-            ...errors,
-            [e.target.name]: err.errors[0],
-          });
-        });
+    console.log(formState);
+  };
 
-        setFormState({
-            ...formState,
-            [e.target.name] : e.target.value,
-        })
+  const signup = (e) => {
+    e.preventDefault();
+    axios
+      .post("https://bw-node.herokuapp.com/auth/register", formState)
+      .then((res) => {
+        console.log(res);
+        history.push("./login");
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("Username is taken");
+      });
+    setFormState(initialState);
+  };
 
-        console.log(formState);
-    }
-    
-    const signup = e => {
-        e.preventDefault();
-        axios.post('https://bw-node.herokuapp.com/auth/register', formState)
-        .then(res =>{
-            console.log(res);
-            history.push('./login')
-        })
-        .catch(err => {
-          console.log(err);
-          alert('Username is taken');
-        });
-        setFormState(initialState);
-    };
-    
-       return ( 
-<div className="blurred-bg-container">
-<Navbar />
-    <div className="content">
-     <div className="text">
-         
-        <div className='signup-form'>
-        <h1 className='title is-2 welcome'>Signup</h1>
-          
-        <form className='form' onSubmit={signup}>
+  return (
+    <div className="blurred-bg-container">
+      <Navbar />
+      <div className="content">
+        <div className="text">
+          <div className="signup-form">
+            <h1 className="title is-2 welcome">Signup</h1>
 
-                <label htmlFor='username'>
-                        Username
-                    <div className="field">
-                        <p className="control has-icons-left">
-                        <input
-                            className='input is-large'
-                            type='text'
-                            name='username'
-                            onChange={onChangeHandler}
-                            value={formState.username}
-                        />
-                        <span className="icon is-small is-left">
-                        <i className="fas fa-user"/>
-                        </span>
-                        </p>
-                        {errors.username}
-                    </div>
-                </label>
-
-                
-                <label htmlFor='email'>
-                        Email
-                    <div className="field">
-                        <p className="control has-icons-left">
-                        <input
-                        className='input is-large'
-                        type='text'
-                        name='email'
-                        onChange={onChangeHandler}
-                        value={formState.email}
+            <form className="form" onSubmit={signup}>
+              <label htmlFor="username">
+                Username
+                <div className="field">
+                  <p className="control has-icons-left">
+                    <input
+                      className="input is-large"
+                      type="text"
+                      name="username"
+                      onChange={onChangeHandler}
+                      value={formState.username}
                     />
-                        <span className="icon is-small is-left">
-                        <i className="fas fa-envelope"/>
-                        </span>
-                        </p>
-                        {errors.email}
-                    </div>
-                </label>
+                    <span className="icon is-small is-left">
+                      <i className="fas fa-user" />
+                    </span>
+                  </p>
+                  {errors.username}
+                </div>
+              </label>
 
-               
+              <label htmlFor="email">
+                Email
+                <div className="field">
+                  <p className="control has-icons-left">
+                    <input
+                      className="input is-large"
+                      type="text"
+                      name="email"
+                      onChange={onChangeHandler}
+                      value={formState.email}
+                    />
+                    <span className="icon is-small is-left">
+                      <i className="fas fa-envelope" />
+                    </span>
+                  </p>
+                  {errors.email}
+                </div>
+              </label>
 
-                <label htmlFor='password'>
-                    Password
-                
-                    <div className="field">
-                        <p className="control has-icons-left">
-                        <input
-                            className='input is-large'
-                            type='password'
-                            name='password'
-                            onChange={onChangeHandler}
-                            value={formState.password}
-                        />
-                        <span className="icon is-small is-left">
-                            <i className="fas fa-lock"/>
-                        </span>
-                        </p>
-                        {errors.password}
-                    </div>
-                </label>
+              <label htmlFor="password">
+                Password
+                <div className="field">
+                  <p className="control has-icons-left">
+                    <input
+                      className="input is-large"
+                      type="password"
+                      name="password"
+                      onChange={onChangeHandler}
+                      value={formState.password}
+                    />
+                    <span className="icon is-small is-left">
+                      <i className="fas fa-lock" />
+                    </span>
+                  </p>
+                  {errors.password}
+                </div>
+              </label>
 
-                <label htmlFor='account_type'>
-                        Account Type
-                        <select
-                          className="input is-large"
-                          type="text"
-                          name="account_type"
-                          onChange={onChangeHandler}
-                          value={formState.account_type}
-                        >
-                          <option></option>
-                          <option value='admin'>Admin</option>
-                        </select>
-                        {errors.account_type}
-                </label>
-
-
-                <div className='spacer-sm'/>
-                <button 
-                    type='submit' 
-                    className='button is-danger is-large is-rounded'
+              <label htmlFor="account_type">
+                Account Type
+                <select
+                  className="input is-large"
+                  type="text"
+                  name="account_type"
+                  onChange={onChangeHandler}
+                  value={formState.account_type}
                 >
-                    Signup
-                </button>
-            </form>
-            </div>
-            </div>
-        <div className="blur"></div>
-    </div>
-</div>
-            
-            )
-    
+                  <option></option>
+                  <option value="admin">Admin</option>
+                </select>
+                {errors.account_type}
+              </label>
 
-}
+              <div className="spacer-sm" />
+              <button
+                type="submit"
+                className="button is-danger is-large is-rounded"
+              >
+                Signup
+              </button>
+            </form>
+          </div>
+        </div>
+        <div className="blur"></div>
+      </div>
+    </div>
+  );
+};
 
 export default SignupForm;
